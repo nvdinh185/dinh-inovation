b47e943e120ed5b494f245c5a3f9050cfe2dc321
hieu.quang <hieu.quang@mobifone.vn>
2020-02-20 11:02:00 +0700

    hieu_them_table_chua_co_dk

--- client/src/app/main-entry/home/home.page.ts
+++ client/src/app/main-entry/home/home.page.ts
@@ -45,6 +45,7 @@ export class HomePage implements OnInit {
       .then(data => {
         // console.log('Data: ', data);
         this.topUsersActions = data;
+        console.log(this.topUsersActions);
       })
       .catch(err => {
         // console.log('Lỗi: ', err);
