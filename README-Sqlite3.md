a62fa3f06800f0dbacc61f4db392df519cfa7322
cuongdq.payment <cuongdq.payment@gmail.com>
2020-02-22 10:49:53 +0700

    add git readme and ifnull(x,y)

--- README-Sqlite3.md
+++ README-Sqlite3.md
@@ -12,7 +12,33 @@ Tất cả các bảng được thiết kế trên excel và dữ liệu tương
 
 # 4. Sử dụng user = abc và password xyz để demo test chương trình
 
-# Hướng dẫn mới cho nhánh mới
+# Hướng dẫn mới cho nhánh mới:
 
-# git push --set-upstream mf3 server-sqlite-demo
+``` sh
+# xem đang ở nhánh nào
+git branch
+# tạo một nhánh mới rẻ từ nhánh đang hiện hữu sang để sửa, thêm, bớt
+git branch <tên nhánh mới rẻ sang>
+# chuyển sang nhánh mới để thực hiện - không ảnh hưởng đến nhánh cũ
+git checkout <tên nhánh mới rẻ sang>
+# đẩy lên máy chủ tên nhánh mới
+git push --set-upstream mf3 <tên nhánh mới rẻ sang>
+
+
+# từ client khác, muốn lấy về thì
+git pull mf3 <tên nhánh mới rẻ sang>/<hoặc bỏ trống sẽ lấy cấu trúc ở header>
+# chuyển sang nhánh mới
+git checkout <tên nhánh mới rẻ sang>
+
+
+
+# Để xóa nhánh không dùng đến nữa dùng lệnh tại máy local
+git branch -d <tên nhánh cần xóa>
+# xóa trên máy chủ git tên nhánh mới này
+git push mf3 --delete <tên nhánh cần xóa>
+
+```
+
+
+
 
--- handlers/idea/sqlite3/list-handler-sqlite3.js
+++ handlers/idea/sqlite3/list-handler-sqlite3.js
@@ -41,42 +41,41 @@ class ListHandler {
     }
 
     getActionsList(req, res, next) {
-        db.getRsts(`
-                    with 
+        db.getRsts(`with 
                     users_origin as
                     (select 
-                    id as user_id
-                    , username
-                    , fullname
-                    , nickname
-                    , avatar
+                        id as user_id
+                        , username
+                        , fullname
+                        , nickname
+                        , avatar
                     from users
                     ),
                     users_ideas as
                     (select 
-                    user_id
-                    , count(1) as count_ideas 
+                        user_id
+                        , count(1) as count_ideas 
                     from ideas
                     group by user_id),
                     users_voted as
                     (select 
-                    user_id
-                    , count(1) as count_voted
+                        user_id
+                        , count(1) as count_voted
                     from ideas_interactives
                     group by user_id),
                     users_commented as
                     (select 
-                    user_id
-                    , count(1) as count_commented
-                    , count(distinct idea_id) as count_commented_ideas 
+                        user_id
+                        , count(1) as count_commented
+                        , count(distinct idea_id) as count_commented_ideas 
                     from ideas_comments
                     group by user_id)
                     select a.* 
-                    , b.count_ideas
-                    , c.count_voted
-                    , d.count_commented
-                    , d.count_commented_ideas
-                    , (count_ideas * 7 + count_commented_ideas * 5 + count_commented * 3 + count_voted) as total_action
+                        , ifnull(b.count_ideas,0) as count_ideas
+                        , ifnull(c.count_voted,0) as count_voted
+                        , ifnull(d.count_commented,0) as count_commented
+                        , ifnull(d.count_commented_ideas,0) as count_commented_ideas
+                        , (ifnull(count_ideas,0) * 7 + ifnull(count_commented_ideas,0) * 5 + ifnull(count_commented,0) * 3 + ifnull(count_voted,0)) as total_action
                     from users_origin a
                     left join users_ideas b
                     on  a.user_id = b.user_id
@@ -85,6 +84,8 @@ class ListHandler {
                     left join users_commented d
                     on  a.user_id = d.user_id
                     order by total_action desc
+                    LIMIT 10
+					OFFSET 0
                     `)
             .then(result => {
                 res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
