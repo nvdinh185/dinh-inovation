a7510a5fb9b4d5daba634b8d0132d385e71b45da
Doan Quoc Cuong <cuongdq.payment@gmail.com>
2020-02-25 19:03:06 +0700

    add login on upgrade

--- client/src/app/admin-entry/upgrade/upgrade.page.ts
+++ client/src/app/admin-entry/upgrade/upgrade.page.ts
@@ -63,6 +63,10 @@ export class UpgradePage implements OnInit {
     }
   }
 
+  // Hàm gọi trang login
+  onClickLogin(){
+    this.router.navigate(['/login']);
+  }
 
   // hàm trả kết quả của form nhập sql thực thi lệnh xong
   onSelectedFinish(evt) {
