4b059379bcfe9b76d3de508176d50152b91eb157
nvdinh185 <nvdinh0766777123@gmail.com>
2020-05-27 15:14:00 +0700

    edit client and server

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: false
   , AUTH_SERVER: 'http://localhost:9223/m-inovation/api/auth'
-  // , AUTH_SERVER: 'https://c3.mobifone.vn/m-inovation/api/auth'
-
   , RESOURCE_SERVER: 'http://localhost:9223/m-inovation/api'
-  // , RESOURCE_SERVER: 'https://c3.mobifone.vn/m-inovation/api'
 };
